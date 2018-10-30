1b8213b754f2c039a233ed8d0cb3bc3d019523e9
wordisthebird <mikeychristie@gmail.com>
2018-10-30 16:43:18 +0000

    CRUD operations complete

--- Secure Software Development/ReadPage.xaml.cs
+++ Secure Software Development/ReadPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -21,6 +22,7 @@ namespace Secure_Software_Development
     /// </summary>
     public partial class ReadPage : Window
     {
+        string str;
         public ReadPage()
         {
             InitializeComponent();
@@ -28,19 +30,62 @@ namespace Secure_Software_Development
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            //open the file explorer on click
-            Process process = new Process();
-            process.StartInfo.UseShellExecute = true;
-            process.StartInfo.FileName = @"C:\Users\S00151977\Desktop\Secure Software Development\Secure Software Development\bin\Debug";
-            process.Start();
+            Text_File_Contents.Clear();
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.DefaultExt = ".txt";
 
 
 
+            if (ofd.ShowDialog() == true)
+            {
 
-            Text_File_Contents.Text = File.ReadAllText("C:/Users/S00151977/Desktop/Secure Software Development/Secure Software Development/bin/Debug/meh.txt");
+                str = ofd.FileName;
 
-            //test dbb
+                Text_File_Contents.Text = File.ReadAllText(str);
+            }
 
+            MessageBox.Show("Here is the file's contents");
+
+
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            StreamWriter SW = new StreamWriter(str);
+
+            SW.WriteLine(Text_File_Contents.Text);
+            SW.Close();
+            MessageBox.Show("File Updated");
+
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this file?",
+                                          "Confirmation",
+                                          MessageBoxButton.YesNo,
+                                          MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                File.Delete(@str);
+                Text_File_Contents.Clear();
+                MessageBox.Show("File deleted");
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+
+            mw.Show();
+
+            this.Close();
         }
     }
 }
